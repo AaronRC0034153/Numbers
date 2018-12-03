1a6af8b6a8f71623fe1c19f2ce5b316694bb571f
Aaron Cheung <rc0034153@student.riversidecollege.ac.uk>
2018-12-03 14:52:36 +0000

    pi and c done

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -18,12 +18,24 @@ namespace Numbers
             double d1 = 5e6;
             box1.AppendText(d1.ToString() + "\n");
             box1.AppendText(d1.ToString("N") + "\n");
-            box1.AppendText(d1.ToString("N3") + "\n");
+            box1.AppendText(d1.ToString("N4") + "\n");
             box1.AppendText(d1.ToString("N0") + "\n");
             box1.AppendText(d1.ToString("0") + "\n");
             box1.AppendText(d1.ToString("0.0") + "\n");
             box1.AppendText(d1.ToString("0.0E0") + "\n");
             box1.AppendText(d1.ToString("0.00E00") + "\n");
+
+            double pi = 3.141592653589793;
+            double c = 299792458;
+            box1.AppendText(pi.ToString("N3") + "\n");
+            box1.AppendText(pi.ToString("N5") + "\n");
+            box1.AppendText(pi.ToString("N15") + "\n");
+            box1.AppendText(c.ToString() + "\n");
+            box1.AppendText(c.ToString("N0") + "\n");
+            box1.AppendText(c.ToString("0.000000000000000E0") + "\n");
+            box1.AppendText(c.ToString("0.00E0") + "\n");
+
+
         }
     }
 }
