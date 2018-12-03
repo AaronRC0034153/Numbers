9b68e9bc90a794a1ffb0e279b2e7e5489619911f
Aaron Cheung <rc0034153@student.riversidecollege.ac.uk>
2018-12-03 14:31:11 +0000

    Add project files.

---
+++ Numbers/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(174, 38);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(400, 321);
+            this.box1.TabIndex = 0;
+            this.box1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.box1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox box1;
+    }
+}
+
